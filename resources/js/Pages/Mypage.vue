f3a1e7faeae7baf71a64e8c3dc62732504b819d0
Song Jae Hyeon <0nocsace@gmail.com>
2021-10-15 21:31:32 +0900

    initial push

---
+++ resources/js/Pages/Mypage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    nice to meet you
+    <button class="btn btn-warning">위험.</button>
+    <button type="button" class="btn btn-primary">asdasd</button>
+  </div>
+</template>
+<script>
+export default {};
+</script>
