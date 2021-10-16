803e45d1c0e962898b5d913cf1a68dc3be1a415a
Song Jae Hyeon <0nocsace@gmail.com>
2021-10-16 14:19:09 +0900

    Inertia test. noproblem

--- resources/js/Pages/Mypage.vue
+++ resources/js/Pages/Mypage.vue
@@ -1,9 +1,5 @@
 <template>
-  <div>
-    nice to meet you
-    <button class="btn btn-warning">위험.</button>
-    <button type="button" class="btn btn-primary">asdasd</button>
-  </div>
+  <div>nice to meet you</div>
 </template>
 <script>
 export default {};
