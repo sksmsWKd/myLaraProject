8b0e90bf4873360a7445b3d2a373d1862233e4eb
Song Jae Hyeon <0nocsace@gmail.com>
2021-10-16 14:18:53 +0900

    2nd semester project.add inertia, ziggy, bootstrap

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
+
+const path = require('path');
+mix.alias({
+    ziggy: path.resolve('vendor/tightenco/ziggy/dist.vue')
+});
+
 mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
