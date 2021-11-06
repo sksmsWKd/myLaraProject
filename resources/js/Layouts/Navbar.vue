3fdf7e9e79876f89a9d94a41b66b0fc966af45a0
Song Jae Hyeon <0nocsace@gmail.com>
2021-11-06 14:20:35 +0900

    add bg-img

--- resources/js/Layouts/Navbar.vue
+++ resources/js/Layouts/Navbar.vue
@@ -1,52 +1,90 @@
 <template>
-  <div class="navbar">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Navbar</a>
+  <div :class="{ navbar: scrollPosition < 100, navbar2: scrollPosition > 100 }">
+    <nav class="d-flex navbar navbar-expand-lg navbar-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarColor02"
+          aria-controls="navbarColor02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#"
-              >Home <span class="sr-only">(current)</span></a
-            >
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-          <li class="nav-item">
-            <a
-              class="nav-link disabled"
-              href="#"
-              tabindex="-1"
-              aria-disabled="true"
-              >Disabled</a
-            >
-          </li>
-        </ul>
+        <div class="collapse navbar-collapse" id="navbarColor02 ">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link active" href="#"
+                >Home
+                <span class="visually-hidden">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                data-bs-toggle="dropdown"
+                href="#"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+                >Dropdown</a
+              >
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Separated link</a>
+              </div>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-sm-2" type="text" placeholder="Search" />
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
       </div>
     </nav>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      scrollPosition: null,
+    };
+  },
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+  },
+};
 </script>
 <style lang="scss">
 .navbar {
-  background-color: black;
+  position: sticky;
+  top: -1;
+}
+
+.navbar2 {
+  position: sticky;
+  top: 0;
 }
 </style>
--- resources/js/Pages/Mypage.vue
+++ resources/js/Pages/Mypage.vue
@@ -1,13 +1,22 @@
 <template>
-  <div style="background-image: url('제목 없음.png')">
-    <navbar />
+  <div>
+    <mainbox></mainbox>
+    <mainbox />
   </div>
 </template>
 <script>
 import Navbar from "@/Layouts/Navbar.vue";
+import Mainbox from "@/Layouts/Mainbox.vue";
+
 export default {
   components: {
     Navbar,
+    Mainbox,
   },
 };
 </script>
+<style lang="scss">
+.navbar {
+  z-index: 1;
+}
+</style>
