e9de7aa37353ba2b43fd170a9bfc71fc251b1eb8
Song Jae Hyeon <0nocsace@gmail.com>
2021-11-29 16:52:42 +0900

    add message

--- resources/js/Pages/Map2.vue
+++ resources/js/Pages/Map2.vue
@@ -8,65 +8,79 @@
 
         mounted() {
 
-            const mapOptions = {
-                center: new naver
+            
+                const mapOptions = {
+                    center: new naver
+                        .maps
+                        .LatLng(38, 125),
+                    zoom: 8,
+                    useStyleMap: true
+                };
+
+                this.map = new naver
                     .maps
-                    .LatLng(38, 125),
-                zoom: 8,
-                useStyleMap: true
-            };
+                    .Map("map", mapOptions);
+                this
+                    .map
+                    .data
+                    .addGeoJson(geojsonfile);
+                this
+                    .map
+                    .data
+                    .setStyle({strokeColor: 'yellow', fillColor: 'yellow'});
 
-            this.map = new naver
-                .maps
-                .Map("map", mapOptions);
-            this
-                .map
-                .data
-                .addGeoJson(geojsonfile);
-            this
-                .map
-                .data
-                .setStyle({strokeColor: 'red', fillColor: 'red'});
+                //폴리곤 생성
+                var contentString = ['<div class="iw_inner">', '   <h3>서울특별시</h3>', '</div>'].join(
+                    ''
+                );
+                //메시지 내용
+                var infowindow = new naver
+                    .maps
+                    .InfoWindow({content: contentString});
+                //메시지 생성
 
-            //폴리곤 생성
+                this
+                    .map
+                    .data
+                    .addListener('mouseover', (e) => {
+                        this
+                            .map
+                            .setCursor("pointer");
+                        var feature = e.feature;
 
-            this
-                .map
-                .data
-                .addListener('mouseover', (e) => {
-                    this
-                        .map
-                        .setCursor("pointer");
-                    var feature = e.feature;
+                        this
+                            .map
+                            .data
+                            .overrideStyle(feature, {
+                                strokeColor: 'red',
+                                fillColor: 'red'
+                            });
 
-                    this
-                        .map
-                        .data
-                        .overrideStyle(feature, {
-                            strokeColor: 'yellow',
-                            fillColor: 'yellow'
-                        });
+                        infowindow.open(this.map, naver.maps.LatLng(37,126));  
 
-                });
+                    });
 
-            this
-                .map
-                .data
-                .addListener('mouseout', (e) => {
-                    var feature = e.feature;
-                    this
-                        .map
-                        .setCursor("auto");
+                this
+                    .map
+                    .data
+                    .addListener('mouseout', (e) => {
+                        var feature = e.feature;
+                        this
+                            .map
+                            .setCursor("auto");
 
-                    this
-                        .map
-                        .data
-                        .overrideStyle(feature, {
-                            strokeColor: 'red',
-                            fillColor: 'red'
-                        });
+                        this
+                            .map
+                            .data
+                            .overrideStyle(feature, {
+                                strokeColor: 'red',
+                                fillColor: 'red'
+                            });
 
-                });
+                    });
+
+                   
+            }
+            
         }
-    }
 </script>
