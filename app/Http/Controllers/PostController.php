c6e5387b88040598d973cac34c1dc035f70f723c
Song Jae Hyeon <0nocsace@gmail.com>
2021-12-01 19:00:16 +0900

    get info from db test

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class PostController extends Controller
@@ -11,6 +12,22 @@ class PostController extends Controller
 
     public function index()
     {
-        return Inertia::render('Index', ['age' => 23]);
+
+        // $a2018 = DB::table('crimes')->select('2014년 계')->where('범죄별', '형법범(건)소계')->get();
+        // $t2018 = json_decode(json_encode($a2018), true);
+        // return Inertia::render('Index', ['age' => $t2018[0]["2014년 계"]]);
+
+
+
+        // $b2018 = DB::table('crimes')->get();
+        // $t2018 = json_decode(json_encode($b2018), true);
+        // dd(array_values($t2018));
+
+
+        $b2018 = DB::table('crimes')->get();
+        $t2018 = json_decode(json_encode($b2018), true);
+        dd($t2018);
+
+        return Inertia::render('Index', ['age' => $t2018]);
     }
 }
